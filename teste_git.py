1549e989461dca1183a00eebe8129fd8d8b62a1b
pedromujica1 <miottopedro@gmail.com>
2023-03-05 11:56:17 -0300

    Dictionary with Star wars sentences

--- teste_git.py
+++ teste_git.py
@@ -3,3 +3,10 @@ print('hello there.')
 lista_teste=[1,2,3,5]
 lista_teste.append(6)
 print(lista_teste)
+
+dic={1: 'hello there',
+    2: 'I have a bad feeling about this',
+    3: 'May the force be with you'}
+
+for i in range(len(dic)):   
+    print(dic[i])
